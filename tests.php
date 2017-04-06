e740c02910ffe8699bc253bfac7a9dca17bbe17b
Kaic <kaicbastidas@gmail.com>
2017-04-06 22:30:48 +0000

    Removed 'league_' on the beggining of file

--- tests.php
+++ tests.php
@@ -3,13 +3,13 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function hasColsNRows_SC_Ascendancies() {
-		require("softcore/sc_ascendancies.php");
+		require("softcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
   	
   	public function hasColsNRows_HC_Ascendancies() {
-		require("hardcore/hc_ascendancies.php");
+		require("hardcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
