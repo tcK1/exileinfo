9673cffadc2d4166d319b4c971aab234d73f1822
Kaic <kaicbastidas@gmail.com>
2017-04-06 22:38:04 +0000

    it should work now

--- tests.php
+++ tests.php
@@ -2,13 +2,13 @@
 
 class tests extends PHPUnit_Framework_TestCase {
 	
-	public function hasColsNRowsSCAscendancies() {
+	public function test_hasColsNRows_SC_Ascendancies() {
 		require("softcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
   	
-  	public function hasColsNRowsHCAscendancies() {
+  	public function test_hasColsNRows_HC_Ascendancies() {
 		require("hardcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
