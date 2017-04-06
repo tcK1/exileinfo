5a26f9d2480435b04f6468c3a9ce74279059660e
Kaic <kaicbastidas@gmail.com>
2017-04-06 22:24:07 +0000

    New test for hardcore ascendancies

--- tests.php
+++ tests.php
@@ -2,8 +2,14 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function hasColsNRows() {
+	public function hasColsNRows_SC_Ascendancies() {
 		require("softcore/sc_ascendancies.php");
+		$this->assertArrayHasKey($array,'cols');
+		$this->assertArrayHasKey($array,'rows');
+  	}
+  	
+  	public function hasColsNRows_HC_Ascendancies() {
+		require("hardcore/hc_ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
