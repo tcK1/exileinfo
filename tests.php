01c72f0abe1f4acc1a6b5ce7a5c34cdee607d0e8
Kaic <kaicbastidas@gmail.com>
2017-04-06 23:14:36 +0000

    new test format

--- tests.php
+++ tests.php
@@ -3,13 +3,13 @@
 class tests extends PHPUnit_Framework_TestCase {
 	
 	public function test_hasColsNRows_SC_Ascendancies() {
-		require("softcore/ascendancies.php");
+		include($_SERVER['DOCUMENT_ROOT']."/softcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
   	
   	public function test_hasColsNRows_HC_Ascendancies() {
-		require("hardcore/ascendancies.php");
+		include($_SERVER['DOCUMENT_ROOT']."/hardcore/ascendancies.php");
 		$this->assertArrayHasKey($array,'cols');
 		$this->assertArrayHasKey($array,'rows');
   	}
