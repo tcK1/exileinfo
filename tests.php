e14da0cc77133c00a580d1468975768b85a06cbd
Kaic <kaicbastidas@gmail.com>
2017-04-07 15:28:55 +0000

    testing the test

--- tests.php
+++ tests.php
@@ -3,15 +3,16 @@ spl_autoload_register('autoloader');
 
 function autoloader($className)
 {
-    include(dirname(__FILE__)."/softcore/ascendancies.php");
-    include(dirname(__FILE__)."/hardcore/ascendancies.php");
+    include(dirname(__FILE__)."/softcore/".$className.".php");
+
 }
 //$this->assertArrayHasKey($array,'rows');
 
 class tests extends PHPUnit_Framework_TestCase {
 	
 	public function test_hasColsNRows_SC_Ascendancies() {
-		//$class = new ascendancies(true);
+		$array = array();
+		ascendancies::get_data_as_array($array);
   	}
   	
   	public function test_hasColsNRows_HC_Ascendancies() {
