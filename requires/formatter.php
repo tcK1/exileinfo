436f6ba21a079defd3d3f75f37a7c19ece3e6d1b
Kaic <kaicbastidas@gmail.com>
2017-04-09 20:39:35 +0000

    testing new test v2

--- requires/formatter.php
+++ requires/formatter.php
@@ -11,13 +11,13 @@ class formatter {
         $array = array();
         $array['cols'] = array(
             // {id: 'task', label: 'Task', type: 'string'}
-            array(id => 'ascendancy', label => 'Ascendancy', type => 'string'),
-            array(id => 'amount', label => 'Amount', type => 'number')
+            array('id' => 'ascendancy', 'label' => 'Ascendancy', 'type' => 'string'),
+            array('id' => 'amount', 'label' => 'Amount', 'type' => 'number')
             );
         $array['rows'] = array();
         foreach ( $this->data as $id => $value ) {
             // {c:[{v: 'Work'}, {v: 11}]}
-            $aux = array(c => array(array(v => $value['Class']), array(v => $value['Count'])));
+            $aux = array('c' => array(array('v' => $value['Class']), array('v' => $value['Count'])));
             array_push($array['rows'], $aux);
         }
         return $array;
