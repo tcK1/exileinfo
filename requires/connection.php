ed76cd3da23c303f92870cd2ef83b2da813f6978
Kaic <kaicbastidas@gmail.com>
2017-04-15 21:55:06 +0000

    made mongodb uri a environment variable

--- requires/connection.php
+++ requires/connection.php
@@ -4,9 +4,7 @@ class connection {
     private $db;
     
     public function __construct() {
-        $string = file_get_contents(dirname(__FILE__)."/credentials.json");
-        $json = json_decode($string, true);
-        $connection = new MongoClient("mongodb://".$json["id"].":".$json["password"]."@ds049486.mlab.com:49486/heroku_869jvsp2");
+        $connection = new MongoClient(getenv("MONGODBURI"));
         $this->db = $connection->heroku_869jvsp2;
     }
     
